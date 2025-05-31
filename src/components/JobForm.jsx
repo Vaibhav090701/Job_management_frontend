04263a55f4d977912571d21ecafa1ba46d29b71b
Vaibhav090701 <vaibhavrandad@gmail.com>
2025-05-31 23:01:19 +0530

    Change in job form2

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -31,12 +31,12 @@ const JobForm = ({ onClose }) => {
   return (
     <div>
       {/* Header */}
-      <div className="flex justify-center items-center mb-8">
+      <div className="flex justify-center items-center mb-3">
         <h2 className="text-2xl font-semibold text-gray-800">Create Job Opening</h2>
       </div>
 
       {/* Form */}
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-1">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           
           {/* Job Title */}
